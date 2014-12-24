689ba52d6a253461ebe36d2ac5e81987a41cd389
jtschoonhoven <jtschoonhoven@gmail.com>
2014-12-23 17:45:28 -0800

    breakthrough, selects hold state when joins change

--- example-app/app.js
+++ example-app/app.js
@@ -39,19 +39,40 @@
 	Backbone.View.prototype.initialize = function(params) {
 		_.extend(this, params);
 		this.childViews = [];
-		this.selected = [];
 		this.listen();
 	};
 
-	Backbone.View.prototype.render = function(el) {
-		if (el) { this.$el = el }
+
+	Backbone.View.prototype.render = function() {
 		this.$el.html(this.template(this));
-		this.onRender();
+		this.trigger('render');
 		return this;
 	};
 
+
 	Backbone.View.prototype.listen = function() {};
-	Backbone.View.prototype.onRender = function() {};
+
+
+	// Call remove recursively on each childView.
+	Backbone.View.prototype.removeChildren = function() {
+    _.each(this.childViews, function(child) {
+      child.removeChildren();
+      child.collection.remove(child.model);
+      Backbone.View.prototype.remove.call(child);
+    });
+    this.childViews = [];
+    return this;
+	};
+
+
+	// Call render on this and every childView in array.
+	Backbone.View.prototype.renderChildren = function() {
+    _.each(this.childViews, function(child) {
+      child.renderChildren();
+      Backbone.View.prototype.render.call(child);
+    });
+    return this;
+	};
 
 
 
@@ -63,12 +84,27 @@
 		template: require('./templates/query-builder.jade'),
 	});
 
-	// Render a joinSet on this.render.
-	QueryBuilder.prototype.onRender = function() {
-		this.joinSet = new JoinSet({ el: '.joins' });
-		this.selectSet = new SelectSet({ el: '.selects' });
-		this.joinSet.render();
+
+	QueryBuilder.prototype.listen = function() {
+		var that = this;
+
+		this.listenTo(this, 'render', function() {
+			that.joinSet = new JoinSet({ 
+				el: '.joins', 
+				isRoot: true, 
+				collection: new Joins() 
+			});
+
+			that.selectSet = new SelectSet({ 
+				el: '.selects', 
+				isRoot: true, 
+				collection: new Selects() 
+			});
+
+			that.joinSet.render();
+		});
 	};
+
 
 	window.qb = new QueryBuilder();
 
@@ -79,7 +115,6 @@
 
 	var InputView = Backbone.View.extend();
 
-	InputView.prototype.onSelect = function() {};
 
 	InputView.prototype.events = {
 		'change select'     : 'selectInput',
@@ -87,18 +122,32 @@
 		'click .remove-btn' : 'removeInput'
 	};
 
+
+	InputView.prototype.removeInput = function(e) {
+		e.stopImmediatePropagation();
+		this.removeChildren().remove();
+		this.collection.remove(this.model);
+
+		var parentIsFieldset = this.parent instanceof Fieldset;
+		var parentIsEmpty = this.parent.childViews.length === 1;
+
+		if (parentIsFieldset && parentIsEmpty) {
+			return this.parent.removeChildren().remove(); 
+		}
+	};
+
+
 	InputView.prototype.addInput = function(e) {
 		e.stopImmediatePropagation();
 
 		if (!this.isRoot) {
-			var targetEl = this.$el.find('.content');
-			console.log(this.el)
-			var fieldset = new this.ParentView({ el: targetEl });
-			fieldset.render();
-			return;
+			var fieldset = new this.ParentView({ 
+				el: this.$el.find('.content'),
+				collection: this.collection, 
+				model: this.model 
+			});
+			return fieldset.render();
 		}
-
-		var targetEl = this.parent.$el.find('.content').first();
 
 		var siblingView = new this.View({
 			ParentView: this.ParentView,
@@ -107,19 +156,18 @@
 			collection: this.collection
 		});
 
+		var targetEl = this.parent.$el.find('.content').first();
 		siblingView.render().$el.appendTo(targetEl);
 		this.parent.childViews.push(siblingView);
 	};
+
 
 	// Get data from each select in group & store in model.
 	InputView.prototype.selectInput = function(e) {
 		e.stopImmediatePropagation();
 		var that = this;
 
-		if (!this.model) { 
-			this.model = new Selection({ id: _.uniqueId() }); 
-			this.collection.add(this.model);
-		}
+		if (!this.model) { this.model = new Selection({ id: _.uniqueId() }); }
 
 		this.$el.find('select').each(function(i) {
 			var select = {
@@ -131,8 +179,8 @@
 			that.model.set(i, select);
 		});
 
+		this.collection.add(this.model);
 		this.render();
-		this.onSelect();
 	};
 
 
@@ -167,10 +215,6 @@
 		template: require('./templates/join.jade')
 	});
 
-	JoinView.prototype.onSelect = function() {
-		qb.selectSet.render();
-	};
-
 	var SelectView = InputView.extend({
 		template: require('./templates/select.jade')
 	});
@@ -187,32 +231,55 @@
 		template: require('./templates/fieldset.jade')
 	});
 
-	Fieldset.prototype.onRender = function() {
-		var targetEl  = this.$el.find('.content').first();
-		var childView = new this.ChildView({ 
-			View: this.ChildView,
-			ParentView: this.View,
-			isRoot: true, 
-			parent: this,
-			collection: this.collection,
+	Fieldset.prototype.listen = function() {
+		var that = this;
+		this.listenTo(this, 'render', function() {
+
+			// If children already exist, render them and return.
+			if (that.childViews.length > 0) {
+				return that.renderChildren();
+			}
+
+			// Else create a new childView and render it.
+			var childView = new that.ChildView({ 
+				View: that.ChildView,
+				ParentView: that.View,
+				isRoot: that.isRoot, 
+				parent: that,
+				collection: that.collection,
+			});
+
+			that.childViews.push(childView);
+			var targetEl = that.$el.find('.content').first();
+			childView.render().$el.appendTo(targetEl);
 		});
-		this.childViews.push(childView);
-		childView.render().$el.appendTo(targetEl);
 	};
+
 
 	var JoinSet = Fieldset.extend({
 		ChildView: JoinView,
-		collection: new Joins(),
+		label: 'Include'
 	});
+
 
 	JoinSet.prototype.View = JoinSet;
 
+
 	var SelectSet = Fieldset.extend({
 		ChildView: SelectView,
-		collection: new Selects()
+		label: 'Select'
 	});
 
+
+	SelectSet.prototype.listen = function() {
+		Fieldset.prototype.listen.call(this);
+		this.listenToOnce(qb.joinSet.collection, 'add', this.render);
+		this.listenTo(qb.joinSet.collection, 'add remove change', this.renderChildren);
+	};
+
+
 	SelectSet.prototype.View = SelectSet;
+
 
 	var FilterSet = Fieldset.extend({});
 	var GroupSet  = Fieldset.extend({});
@@ -222,10 +289,7 @@
 	// Start app
 	// ==================================================
 
-	tables.on('sync', function() { 
-		qb.render(); 
-		qb.joinSet.render();
-	});
+	tables.on('sync', function() { qb.render(); });
 	tables.fetch();
 
 })()
