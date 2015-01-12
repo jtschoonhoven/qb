6aca5e029ba0e4e4abf8746fcea1c0fc0075f45c
jtschoonhoven <jtschoonhoven@gmail.com>
2015-01-12 11:59:05 -0800

    more testing for GROUP BY

--- test/group-by.test.js
+++ test/group-by.test.js
@@ -3,23 +3,50 @@ var mocha       = require('mocha')
 ,   expect      = require('chai').expect
 ,   Qb          = require('../lib');
 
-describe('groupBy.test.js', function() {
+describe('group-by.test.js', function() {
+
+	var def = { users: { columns: ['id', 'name'], as: 'Users' } };
+	var qb  = new Qb(def);
 
 
-	describe('Add a GROUP BY clause', function() {
+	describe('Add an GROUP BY clause from spec.groupBy', function() {
 
-		var def = { users: { columns: ['id', 'name'], as: 'Users' } };
-		var qb  = new Qb(def);
-		var sql = 'SELECT "Users"."name", COUNT("Users"."id") AS "num" FROM "users" AS "Users" GROUP BY "Users"."name"';
-
-		it('from the selects array', function() {
-			var spec  = { select: [{ name: 'name', groupBy: true }, { name: 'id', function: 'COUNT', as: 'num' }], from: 'users' };
+		it('passing a column name as a string', function() {
+			var spec  = { select: 'id', from: 'users', groupBy: 'id' };
+			var sql   = 'SELECT "Users"."id" FROM "users" AS "Users" GROUP BY "Users"."id"';
 			var query = qb.query(spec);
 			expect(query.string).to.equal(sql);
 		});
 
-		it('from the groupBy property', function() {
-			var spec  = { select: [{ name: 'name' }, { name: 'id', function: 'COUNT', as: 'num' }], from: 'users', groupBy: 'name' };
+		it('passing a string array of column names', function() {
+			var spec  = { select: ['id', 'name'], from: 'users', groupBy: ['id', 'name'] };
+			var sql   = 'SELECT "Users"."id", "Users"."name" FROM "users" AS "Users" GROUP BY "Users"."id", "Users"."name"';
+			var query = qb.query(spec);
+			expect(query.string).to.equal(sql);
+		});
+
+		it('passing an object', function() {
+			var spec  = { select: 'id', from: 'users', groupBy: { name: 'id' } };
+			var sql   = 'SELECT "Users"."id" FROM "users" AS "Users" GROUP BY "Users"."id"';
+			var query = qb.query(spec);
+			expect(query.string).to.equal(sql);
+		});
+
+		it('passing an array of objects', function() {
+			var spec  = { select: ['id', 'name'], from: 'users', groupBy: [{ name: 'id' }, { name: 'name'}] };
+			var sql   = 'SELECT "Users"."id", "Users"."name" FROM "users" AS "Users" GROUP BY "Users"."id", "Users"."name"';
+			var query = qb.query(spec);
+			expect(query.string).to.equal(sql);
+		});
+
+	});
+
+
+	describe('Add an GROUP BY clause from spec.select', function() {
+
+		it('passing a boolean', function() {
+			var spec  = { select: { name: 'id', groupBy: true }, from: 'users' };
+			var sql   = 'SELECT "Users"."id" FROM "users" AS "Users" GROUP BY "Users"."id"';
 			var query = qb.query(spec);
 			expect(query.string).to.equal(sql);
 		});
